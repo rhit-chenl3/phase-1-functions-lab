e3ef5ad7b392530ddc4fe6e7a106965bbaec2854
Leo Chen <chenl3@rose-hulman.edu>
2021-09-20 18:17:20 -0400

    commit tree 0bdcefdd95b7b78bc797032457ef3b5ebe3b0f8e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+    return Math.abs(someValue - 42);
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    const newValue = distanceFromHqInBlocks(someValue);
+    // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+    return newValue * 264;
+  }
+
+  function distanceTravelledInFeet(startingBlock, endingBlock) {
+    return Math.abs(startingBlock - endingBlock) * 264;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distanceFeet = distanceTravelledInFeet(start, destination);
+    if (distanceFeet <= 400){
+        return 0;
+    } else if(distanceFeet <= 2000){
+        return 0.02 * (distanceFeet - 400);
+    } else if (distanceFeet <= 2500){
+        return 25;
+    } else{
+        return 'cannot travel that far';
+    }
+  }
